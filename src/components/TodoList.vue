c8939dc387f551b780854765747435d46f9c83ee
zscholes18 <461104069@qq.com>
2022-01-05 16:13:56 +0800

    project init

---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="todo-list">
+        <div>
+          <input type="text" :value="inputValue" @input="setInput">
+          <button @click="add">add</button>  
+        </div>
+        
+        <list-item v-for="item in listData" :title="item" :checked="false" @onCheck="onCheck"></list-item>
+    </div>
+</template>
+<script setup>
+    import { ref } from 'vue';
+import ListItem from './TodoList/ListItem.vue';
+    const inputValue=ref('');
+    const listData = ref([]);
+    function setInput (e) {
+        inputValue.value = e.target.value
+        
+    }
+    function add () {
+        if(inputValue.value.length>0) {
+            listData.value.push(inputValue.value);
+            inputValue.value=''
+        }
+    }
+    const onCheck = (e) => {
+        console.log(e)
+    }
+</script>
---
+++ src/components/TodoList/ListItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <label>
+        {{title}}
+        <input type="checkbox" :value="checked" @click="click">
+    </label>
+</template>
+<script setup>
+    const props = defineProps({
+        title: {
+            type: String,
+            default: ''
+        },
+        checked: {
+            type: Boolean,
+            default: false
+        }
+    })
+    const emit = defineEmits([
+        'onCheck'
+    ])
+    const click = (e) => {
+        emit('onCheck', e);
+    }
+</script>
